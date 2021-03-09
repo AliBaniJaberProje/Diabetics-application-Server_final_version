96bf0bcbae86292fcea2dae1cbf7a8621a5f1f73
AliBaniJaberProje <alibanijaber1999@gmail.com>
2021-03-09 04:45:40 +0200

    even

--- serverSettings.js
+++ serverSettings.js
@@ -19,7 +19,7 @@ export const allowLevelTwo=(req,res,next)=>{
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
     res.setHeader( 'Access-Control-Allow-Headers', 'Accept,Accept-Language,Content-Language,Content-Type');
 
-
+    next();
     // res.setHeader("Access-Control-Allow-Origin", "*");
     // res.setHeader(
     //     "Access-Control-Allow-Headers",
@@ -29,7 +29,7 @@ export const allowLevelTwo=(req,res,next)=>{
     //     "Access-Control-Allow-Methods",
     //     "GET, POST, PATCH, PUT, DELETE, OPTIONS"
     // );
-     next();
+
 
 
 }
