fc1a765ae88b9d972ae2b56fa46e13101042cc3f
AliBaniJaberProje <alibanijaber1999@gmail.com>
2021-03-10 04:37:23 +0200

    even

--- routes/image-upload.js
+++ routes/image-upload.js
@@ -2,9 +2,8 @@ import express from 'express'
 const router = express.Router()
 import * as isAuthorisedUser from '../middleware/auth/isAuthorisedUser.js'
 import multer from "multer";
-import path from "path";
-const moduleURL = new URL(import.meta.url);
-const __dirname = path.dirname(moduleURL.pathname);
+import jwt from "jsonwebtoken"
+import patient from "../model/users/patient.js";
 
 const storagePatient = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -18,8 +17,10 @@ const updlode = multer({
     storage: storagePatient
 })
 
-router.post("/patient",isAuthorisedUser.isAuthorisedPatient,updlode.single('img'),(req,res,_)=>{
-    console.log(req.file)
+router.post("/patient",isAuthorisedUser.isAuthorisedPatient,updlode.single('img'),async(req,res,_)=>{
+    const resultJWT=await jwt.decode(req.headers["x-auth-token"])
+
+    await patient.findByIdAndUpdate({id:resultJWT.id},{imgURL:"https://jaber-server.herokuapp.com/"+"images/patient/"+req.file.filename})
     res.status(200).json({
         msg:"success",
         'imgUrl':"https://jaber-server.herokuapp.com/"+"images/patient/"+req.file.filename
