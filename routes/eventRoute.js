8b25fc7e9791e64d6069e6960f3705b1819cec25
AliBaniJaberProje <alibanijaber1999@gmail.com>
2021-04-01 04:22:35 +0300

    info patient

--- routes/eventRoute.js
+++ routes/eventRoute.js
@@ -5,12 +5,12 @@ import * as isAuthorisedUser from '../middleware/auth/isAuthorisedUser.js'
 const router = express.Router()
 router.post("/",isAuthorisedUser.isAuthorisedDoctor,eventServesHandler.addNewEvent)
 router.get("/",isAuthorisedUser.isAuthorisedDoctor,eventServesHandler.getAllEventToDoctor)
-router.delete("/",isAuthorisedUser.isAuthorisedDoctor,eventServesHandler.deleteEventFromDoctor)
+router.delete("/:id",isAuthorisedUser.isAuthorisedDoctor,eventServesHandler.deleteEventFromDoctor)
 router.get("/myEvent",isAuthorisedUser.isAuthorisedPatient,eventServesHandler.getEventWhereUserId)
 router.post("/selectEvent",isAuthorisedUser.isAuthorisedPatient,eventServesHandler.selectEventForUser)
 router.get("/getMyEvent",isAuthorisedUser.isAuthorisedPatient,eventServesHandler.getMyEvent)
 router.post("/AvailableEvent",isAuthorisedUser.isAuthorisedPatient,eventServesHandler.getAllAvailableEvent)
-router.delete("/:id",/*isAuthorisedUser.isAuthorisedPatient,*/eventServesHandler.deleteEvent)
+router.delete("/:id",isAuthorisedUser.isAuthorisedPatient,eventServesHandler.deleteEvent)
 
 
 export default router
