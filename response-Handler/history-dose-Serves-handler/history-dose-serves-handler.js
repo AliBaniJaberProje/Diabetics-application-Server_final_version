42dd1543ace9b31dcd9c3c45d2c60f70f8771052
AliBaniJaberProje <alibanijaber1999@gmail.com>
2021-04-03 03:16:51 +0300

    request info

--- response-Handler/history-dose-Serves-handler/history-dose-serves-handler.js
+++ response-Handler/history-dose-Serves-handler/history-dose-serves-handler.js
@@ -7,20 +7,20 @@ const getHistoryDosesToDoctor=async (req,res,_)=>{
         res.status(200).json(dataForThisUser)
 
         console.log(req.params)
-        const startDate=new Date(Number(req.params['year']),Number(req.params['month'])-1,0,0,0,0,0)
-        const endDate=new Date(Number(req.params['year']),Number(req.params['month'])-1,31,23,59,59,59)
-        let resultReading=await doseHistory.find({$and:[{date: { $gte: startDate, $lte: endDate }},{idPatient:req.params.id}]})
-        resultReading=[]
-        if(resultReading.length!=0){
-            res.status(200).json(resultReading)
-        }else{
-            let result=[]
-            const dataForThisUser=await doseHistory.find({idPatient:req.params.id}).sort({endDate:true})
-            res.status(200).json(dataForThisUser)
-            // while (true){
-            //
-            // }
-        }
+        // const startDate=new Date(Number(req.params['year']),Number(req.params['month'])-1,0,0,0,0,0)
+        // const endDate=new Date(Number(req.params['year']),Number(req.params['month'])-1,31,23,59,59,59)
+        // let resultReading=await doseHistory.find({$and:[{date: { $gte: startDate, $lte: endDate }},{idPatient:req.params.id}]})
+        // resultReading=[]
+        // if(resultReading.length!=0){
+        //     res.status(200).json(resultReading)
+        // }else{
+        //     let result=[]
+        //     const dataForThisUser=await doseHistory.find({idPatient:req.params.id}).sort({endDate:true})
+        //     res.status(200).json(dataForThisUser)
+        //     // while (true){
+        //     //
+        //     // }
+    //    }
 
     }catch (e) {
         console.log(e.message)
