8d236674097ec3de403fd5311f947c4a528cefc0
AliBaniJaberProje <alibanijaber1999@gmail.com>
2021-04-03 03:50:02 +0300

    request info

--- response-Handler/history-dose-Serves-handler/history-dose-serves-handler.js
+++ response-Handler/history-dose-Serves-handler/history-dose-serves-handler.js
@@ -6,8 +6,10 @@ const getHistoryDosesToDoctor=async (req,res,_)=>{
 
         const startDate=new Date(Number(req.params['year']),Number(req.params['month'])-1,0,0,0,0,0)
         const endDate=new Date(Number(req.params['year']),Number(req.params['month'])-1,31,23,59,59,59)
-        let resultReading=await doseHistory.find({$and:[{startDate: { $gte: startDate, $lte: endDate }},{idPatient:req.params.id}]})
-
+        let resultReading=await doseHistory.find({startDate: { $gte: startDate, $lte: endDate }})
+        console.log("_______________________________________")
+        console.log(resultReading)
+        console.log("________________________________________")
         if(resultReading.length!=0){
             res.status(200).json(resultReading)
         }else{
