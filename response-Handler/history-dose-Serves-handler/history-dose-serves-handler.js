c0bcb01df70be6f501468423b8f3e728391b81c1
AliBaniJaberProje <alibanijaber1999@gmail.com>
2021-04-03 03:44:10 +0300

    request info

--- response-Handler/history-dose-Serves-handler/history-dose-serves-handler.js
+++ response-Handler/history-dose-Serves-handler/history-dose-serves-handler.js
@@ -3,25 +3,20 @@ import doseHistory from "../../model/doseHistory.js"
 const getHistoryDosesToDoctor=async (req,res,_)=>{
 
     try {
-        const dataForThisUser=await doseHistory.find({"doseItem.idPatient":req.params.id}).sort({endDate:-1})
-        console.log(dataForThisUser)
 
-        res.status(200).json(dataForThisUser)
+        const startDate=new Date(Number(req.params['year']),Number(req.params['month'])-1,0,0,0,0,0)
+        const endDate=new Date(Number(req.params['year']),Number(req.params['month'])-1,31,23,59,59,59)
+        let resultReading=await doseHistory.find({$and:[{date: { $gte: startDate, $lte: endDate }},{idPatient:req.params.id}]})
 
-        // const startDate=new Date(Number(req.params['year']),Number(req.params['month'])-1,0,0,0,0,0)
-        // const endDate=new Date(Number(req.params['year']),Number(req.params['month'])-1,31,23,59,59,59)
-        // let resultReading=await doseHistory.find({$and:[{date: { $gte: startDate, $lte: endDate }},{idPatient:req.params.id}]})
-        // resultReading=[]
-        // if(resultReading.length!=0){
-        //     res.status(200).json(resultReading)
-        // }else{
-        //     let result=[]
-        //     const dataForThisUser=await doseHistory.find({idPatient:req.params.id}).sort({endDate:true})
-        //     res.status(200).json(dataForThisUser)
-        //     // while (true){
-        //     //
-        //     // }
-    //    }
+        if(resultReading.length!=0){
+            res.status(200).json(resultReading)
+        }else{
+
+            const dataForThisUser=await doseHistory.find({"doseItem.idPatient":req.params.id}).sort({endDate:-1})
+            console.log(dataForThisUser)
+            res.status(200).json(dataForThisUser[0])
+
+       }
 
     }catch (e) {
         console.log(e.message)
