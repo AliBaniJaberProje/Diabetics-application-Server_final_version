3fa049ef07881d6118dfb573493ab7dfbb04c64a
AliBaniJaberProje <alibanijaber1999@gmail.com>
2021-04-13 05:33:50 +0300

    request info

--- AdaptadorFoodServes/food-serves-handler.js
+++ AdaptadorFoodServes/food-serves-handler.js
@@ -2,7 +2,7 @@ import fetch from  "node-fetch"
 
 import Patient from "../model/users/patient.js"
 import food_history from "../model/food-history.js"
-
+import dose from "../model/dose.js"
 import food from "../model/food.js"
 import jwt from "jsonwebtoken";
 const getAllFood=async(req,res,_)=>{
@@ -35,7 +35,7 @@ const findFoodCategory=async(req,res,_)=>{
         const token=req.headers["x-auth-token"];
         const resultJWTDecode=await jwt.decode(token)
         const nowDate=new Date()
-        const userInfo=await Patient.findOne({id:resultJWTDecode.id}).select({diabetesType:true,weight:true,height:true,birthDate:true,_id:false})
+        const userInfo=await Patient.findOne({id:resultJWTDecode.id}).select({diabetesType:true,weight:true,height:true,birthDate:true,_id:false,gender:true})
         const age=nowDate.getFullYear()-(new Date(userInfo["birthDate"])).getFullYear()
 
         const startDate=new Date(nowDate.getFullYear() , nowDate.getMonth(), nowDate.getDate() , 0,0,0,0,)
@@ -77,6 +77,7 @@ const findFoodCategory=async(req,res,_)=>{
         var pro=0
         var fat=0
         var en=0
+
         console.log(foodThatImEat)
         for(var i = 0  ; i<foodThatImEat.length;i++){
             ch+=Number(foodThatImEat[i]["FoodNutrients"]["Carbohydrate"]["value"])*(foodThatImEat[i]["amount"]) /100
@@ -89,8 +90,51 @@ const findFoodCategory=async(req,res,_)=>{
         console.log(pro)
         console.log(fat)
         console.log(en)
+        console.log(age)
+        console.log(userInfo["height"])
+        console.log(userInfo["weight"])
+        console.log(userInfo["diabetesType"])
+        console.log(userInfo["gender"])
+        let foods;
+        let toFilterCh;
+        if(userInfo["diabetesType"]=="أ"){
+            console.log("im A")
+            var TDD=0
+            const myDose=await dose.findOne({idPatient:resultJWTDecode.id})
+            for(var t=0;t<myDose["inject"].length;t++){
+                if(myDose["inject"][t]["flag"]==0 || myDose["inject"][t]["flag"]==2){
+                    TDD+=Number(myDose["inject"][t]["amountOfinj"])
+                }
+            }
+            console.log("TDD")
+            console.log(TDD)
+            const rTDD=Math.round(500/TDD)
+            const allCH=rTDD*TDD /// 780g CH
+            //const totalCAL=allCH*4
 
-        const foods=await food.find({"category":req.params.foodCategory},).sort({name:1})
+             toFilterCh= (allCH - ch);
+
+           // console.log(toFilterCh)
+            // foods=await food.find({$and:[{"category":req.params.foodCategory},{"FoodNutrients.Carbohydrate.value":{  $lte: (toFilterCh+5) }}]}).sort({name:1})
+            //console.log(foods)
+
+
+        }else if(userInfo["diabetesType"]=="ب")
+        {
+
+            console.log("im B")
+             let BMR=0;
+            if(userInfo["gender"]=="ذكر"){
+                BMR=(13.75*Number(userInfo["weight"]))+(5*Number(userInfo["height"]))-(6.76*Number(age))+66
+            }else if(userInfo["gender"]=="انثى"){
+                BMR=(9.6*Number(userInfo["weight"]))+(1.8*Number(userInfo["height"]))-(4.7*Number(age))+655
+            }
+             toFilterCh=Math.round(BMR/4) * .65
+
+             toFilterCh=toFilterCh-ch
+        }
+        console.log(toFilterCh)
+        foods=await food.find({$and:[{"category":req.params.foodCategory},{"FoodNutrients.Carbohydrate.value":{  $lte: (toFilterCh+5) }}]}).sort({name:1})
 
         var result=[];
 
