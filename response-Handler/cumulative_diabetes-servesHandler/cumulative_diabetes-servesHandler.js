c2da475dc256f69c8e6880d74cf3bcb4e2169f4c
AliBaniJaberProje <alibanijaber1999@gmail.com>
2021-04-14 07:45:40 +0300

    request info

--- response-Handler/cumulative_diabetes-servesHandler/cumulative_diabetes-servesHandler.js
+++ response-Handler/cumulative_diabetes-servesHandler/cumulative_diabetes-servesHandler.js
@@ -14,6 +14,7 @@ const addNewCumulative =async (req,res,_)=>{
         })
 
     }catch (e) {
+        console.log(e.message)
         res.status(404).json({
             "error":e.message
         })
