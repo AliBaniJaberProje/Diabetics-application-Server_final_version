84cb1934b6040e878284bd3f5adba7097ed01821
AliBaniJaberProje <alibanijaber1999@gmail.com>
2021-04-18 07:36:05 +0300

    request info

--- response-Handler/event_history_servesHandler/event_history_servesHandler.js
+++ response-Handler/event_history_servesHandler/event_history_servesHandler.js
@@ -2,9 +2,9 @@ import eventHistory from "../../model/eventHistory.js";
 
 
 const getEventHistoryToDoctor=async (req,res,_)=>{
-    res.status(200).json({
-        "msg":"erororrprprp"
-    })
+
+
+
 }
 
 export {
