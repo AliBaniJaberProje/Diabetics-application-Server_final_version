6b312bd238675c29963e1cd4e0a5310f7f53c9b2
AliBaniJaberProje <alibanijaber1999@gmail.com>
2021-05-01 14:03:09 +0300

    done vf0

--- response-Handler/update_password/passwprd_serves_handler.js
+++ response-Handler/update_password/passwprd_serves_handler.js
@@ -91,10 +91,10 @@ const updatePasswordPatientNotForgetIt=async (req,res,_)=>{
 
 const sendCodeToDoctor=async (req,res,_)=>{
     try{
+
         const randomCode=getRandom()
-        const doctorinfo=await doctor.updateOne({id:req.body.id,phoneNumber:req.body.phoneNumber},{$set:{code:randomCode}})
-        if(doctorinfo!=null){
-            console.log(doctorinfo)
+        const patientInfo=await doctor.findOneAndUpdate({id:req.body.id,phoneNumber:req.body.phoneNumber},{$set:{pinCode:randomCode}})
+        if(patientInfo!=null){
             res.status(200).json({
                 "code":randomCode,
             })
