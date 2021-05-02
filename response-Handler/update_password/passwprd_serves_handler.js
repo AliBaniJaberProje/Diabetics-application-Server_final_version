3a14d56f3082b8470855d070d0e5a3aa93f94db0
AliBaniJaberProje <alibanijaber1999@gmail.com>
2021-05-02 05:52:58 +0300

    done vf0

--- response-Handler/update_password/passwprd_serves_handler.js
+++ response-Handler/update_password/passwprd_serves_handler.js
@@ -136,7 +136,7 @@ const updatePasswordDoctorNotForgetIt=async (req,res,_)=>{
             }
         }
         else{
-            res.status(404).json({
+            res.status(200).json({
                 "msg":"العملية خاطئ "
             })
         }
