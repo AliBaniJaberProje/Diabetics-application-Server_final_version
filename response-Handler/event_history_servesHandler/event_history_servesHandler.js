150e7a3e41d8271f3a1378523b7f1b7469805665
AliBaniJaberProje <alibanijaber1999@gmail.com>
2021-05-12 01:34:21 +0300

    done vf0

--- response-Handler/event_history_servesHandler/event_history_servesHandler.js
+++ response-Handler/event_history_servesHandler/event_history_servesHandler.js
@@ -63,7 +63,10 @@ const event=async (req,res,_)=>{
 
 const eventHistoryCount=async (req,res,_)=>{
     try {
+        console.log("************************")
         console.log(req.headers)
+        console.log("************************")
+
         const token=req.headers.authorization.split(" ")[1]
         const resultDecodeJWT=  jwt.decode(token);
         const toFindNumberOfDays=new Date(Number(req.params.year),Number(req.params.month),0,0,0,0,0)
