2cc228ee23983923c08e50b76468e199acb73737
AliBaniJaberProje <alibanijaber1999@gmail.com>
2021-05-15 15:07:03 +0300

    done vf0

--- response-Handler/dailyReadingServes-handler/dailyReadingServes-handler.js
+++ response-Handler/dailyReadingServes-handler/dailyReadingServes-handler.js
@@ -124,9 +124,11 @@ const getInformationDailyReadingAtThisDay=async (req,res,_)=>{
 
 const getDailyReadingToDoctor=async (req,res,_)=>{
     try {
+        const toFindNumberOfDays=new Date(Number(req.params.year),Number(req.params.month),0,0,0,0,0)///شرفي هان تغير
+
         console.log(req.params)
        const startDate=new Date(Number(req.params['year']),Number(req.params['month'])-1,1,0,0,0,0)
-        const endDate=new Date(Number(req.params['year']),Number(req.params['month'])-1,31,23,59,59,59)
+        const endDate=new Date(Number(req.params['year']),Number(req.params['month'])-1,toFindNumberOfDays.getDate(),23,59,59,59)
        const resultReading=await dailyReadingModel.find({$and:[{date: { $gte: startDate, $lte: endDate }},{idPatient:req.params.id}]}).sort({startDate:1})
         res.status(200).json(resultReading)
     }catch (e) {
